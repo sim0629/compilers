12862b6d069a55bc47ac9c763011e6fdcf93e3d0
Gyumin Sim <sim0629@gmail.com>
2016-06-05 22:11:53 +0900

    Remove a redundant return statement

--- snuplc/src/backend.cpp
+++ snuplc/src/backend.cpp
@@ -427,7 +427,6 @@ string CBackendx86::Label(const CTacLabel* label) const
   ostringstream o;
   o << "l_" << cs->GetName() << "_" << label->GetLabel();
   return o.str();
-  return "l_" + cs->GetName() + "_" + label->GetLabel();
 }
 
 string CBackendx86::Label(string label) const
