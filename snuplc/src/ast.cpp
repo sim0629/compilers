6e7f5648e386aa201bd3fe08082b792cab02e251
Gyumin Sim <sim0629@gmail.com>
2016-06-06 15:22:07 +0900

    Change '#' to '$' for names of string constant

--- snuplc/src/ast.cpp
+++ snuplc/src/ast.cpp
@@ -1902,7 +1902,7 @@ CAstStringConstant::CAstStringConstant(CToken t, const string value,
   _value = new CDataInitString(value);
 
   ostringstream o;
-  o << "_str_#" << ++_idx;
+  o << "_str_$" << ++_idx;
 
   _sym = new CSymGlobal(o.str(), _type);
   _sym->SetData(_value);
